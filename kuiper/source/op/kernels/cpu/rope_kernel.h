0b7bef2b99bb5cbe561d8398a13aee46738951e0
GaoXiangYa <gx19991105@outlook.com>
2024-07-28 14:23:32 +0800

    build kuiper from scratch

--- kuiper/source/op/kernels/cpu/rope_kernel.h
+++
@@ -1,9 +0,0 @@
-#ifndef LLAMA_INFER_ROPE_KERNEL_H
-#define LLAMA_INFER_ROPE_KERNEL_H
-#include "tensor/tensor.h"
-namespace kernel {
-void rope_kernel_cpu(int32_t dim, int32_t kv_dim, int32_t head_size,
-                     const tensor::Tensor& input_q, const tensor::Tensor& input_k,
-                     const tensor::Tensor& input_pos, void* stream = nullptr);
-}  // namespace kernel
-#endif  // LLAMA_INFER_ROPE_KERNEL_H
